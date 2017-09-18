62f595463426bed32d8ff2228603e790abe952fd
maxi <max1y0@live.com.ar>
2017-09-18 19:35:25 -0300

    changed containers to divs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 for (var i = 0; i<9; i++){
-	document.getElementsByClassName('tablero').insertAdjacentHTML('afterend','<container class="Region" id='+i +'></container> ');
+	document.getElementsByClassName('tablero').insertAdjacentHTML('afterend','<div class="Region" id='+i +'></div> ');
 	for (var j =0; j<9; j++){
-		document.getElementById(i).insertAdjacentHTML('afterend','<container class="Celda" id='+ i + '-' + j + '></container> ');
+		document.getElementById(i).insertAdjacentHTML('afterend','<div class="Celda" id='+ i + '-' + j + '></div> ');
 	}
 }
