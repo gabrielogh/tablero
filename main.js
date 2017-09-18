5c34f96d0c1dcf0c3e01e958d41730adcab52e79
maxi <max1y0@live.com.ar>
2017-09-18 19:49:37 -0300

    agregado input text a las celdas

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 for (var i = 0; i<9; i++){
-	document.getElementById('tablero').innerHTML ='<div class="Region" id='+i +'></div> ';
+	document.getElementById('tablero').insertAdjacentHTML('afterend','<div class="Region" id='+i +'></div> ');
 	for (var j =0; j<9; j++){
-		document.getElementById(i).insertAdjacentHTML('afterend','<div class="Celda" id='+ i + '-' + j + '></div> ');
+		document.getElementById(i).insertAdjacentHTML('afterend','<div class="Celda" id='+ i + '-' + j + '> <input type="text"> </div>');
 	}
 }
