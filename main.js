74a0c721cd353e96ab7484a732ade85258b22d18
maxi <max1y0@live.com.ar>
2017-09-18 19:54:39 -0300

    fix arreglada la estructura de las regiones y celdas

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 for (var i = 0; i<9; i++){
 	document.getElementById('tablero').insertAdjacentHTML('beforeend','<div class="Region" id='+i +'></div> ');
 	for (var j =0; j<9; j++){
-		document.getElementById(i).insertAdjacentHTML('afterend','<div class="Celda" id='+ i + '-' + j + '> <input type="text"> </div>');
+		document.getElementById(i).insertAdjacentHTML('beforeend','<div class="Celda" id='+ i + '-' + j + '> <input type="text"> </div>');
 	}
 }
