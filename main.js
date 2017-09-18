e7b08f8fc9afad3f1c879e50556ec46a1cc9336c
maxi <max1y0@live.com.ar>
2017-09-18 19:40:58 -0300

    fixed errors on main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 for (var i = 0; i<9; i++){
-	document.getElementsByClassName('tablero').insertAdjacentHTML('afterend','<div class="Region" id='+i +'></div> ');
+	document.getElementById('tablero').innerHTML ='<div class="Region" id='+i +'></div> ';
 	for (var j =0; j<9; j++){
 		document.getElementById(i).insertAdjacentHTML('afterend','<div class="Celda" id='+ i + '-' + j + '></div> ');
 	}
