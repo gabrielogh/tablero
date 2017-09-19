897668492f1d75c9ac7a611b42ad729fb8a1b268
gabrielogh <gabriel.ogh@gmail.com>
2017-09-19 10:50:47 -0300

    no se usa

--- length.js
+++
@@ -1,8 +0,0 @@
-
-  // This is an old version, for a more recent version look at
-  // https://jsfiddle.net/DRSDavidSoft/zb4ft1qq/2/
-  function maxLengthCheck(object)
-  {
-    if (object.value.length > object.maxLength)
-      object.value = object.value.slice(0, object.maxLength)
-  }
